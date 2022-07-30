29b1b02e4ddde08041b195c126b48d24901ebc81
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2022-07-30 11:06:51 +0600

    python doc1

---
+++ Hello.py
@@ -0,0 +1,72 @@
+'''
+print("Hello Ai \nI wanna be a Ai Developer") use \n for new line
+Output: Hallo Ai 
+        I wanna be a Ai Developer
+
+print("Hello Ai \tI wanna be a Ai Developer") \t means 4 speech 
+Output: Hello Ai    I wanna be a Ai Developer
+
+print("\"Hello Ai\"") use \""\ for print a string with double Quotation
+Output: "Hello Ai"
+
+'''
+# single comment
+'''
+Multiple line comment
+Multiple line comment
+Multiple line comment
+
+'''
+
+name = "sajeeb"  # string variable
+age = 22  # data type variable
+cgpa = 3.16
+
+print("I here it from " + name)  # use + for string
+# output: I here it from sajeeb mandal raz
+
+print("He is " + name)
+# output: He is sajeeb mandal raz
+
+print(name + " is ", age, " years old")  # Don't use + when it is a data type!!
+# output: sajeeb mandal raz is  22  years old
+
+print(name + " get", cgpa, "in last exam")
+# output: sajeeb get 3.16 in last exam
+
+# Numerical operations
+print(15 + 5)  # addition
+print(15 - 5)  # subtraction
+print(15 * 5)  # multiplication
+print(15 / 4)  # division alwayse show floating number
+print(15 % 4)  # divide up
+print(27//4)  # for flor, flor means 3.6 here, 3 is flor and 6 is sling
+print(4 ** 3)  # for power like 2^2, 4^3 use **
+
+# Input from user
+# when need to get input from user use input()
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+cgpa = input("Enter your cgpa: ")
+
+print("Student Information")
+print("--------------------")
+print("Name: "+name)
+print("Age: "+age)
+print("CGPA: "+cgpa)
+'''
+Input From user:
+
+Enter your name: sajeeb mandal
+Enter your age: 22
+Enter your cgpa: 3.16
+
+Output
+Student Information
+--------------------
+Name: sajeeb mandal
+Age: 22
+CGPA: 3.16
+
+
+'''
